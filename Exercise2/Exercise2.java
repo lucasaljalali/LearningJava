7279ab9f911ea53c2f51d44e023e3c7d40a3b128
Lucas al Jalali <lucasaljalali@gmail.com>
2022-09-20 22:49:09 +0100

    Exercise 4

--- Exercise2/Exercise2.java
+++ Exercise2/Exercise2.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class Exercise2 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            int n1 = 0, n2 = 0, res = 0;
 
-        int n1 = 0, n2 = 0, res = 0;
+            System.out.println("Type a integer: ");
+            n1 = scan.nextInt();
 
-        System.out.println("Type a integer: ");
-        n1 = scan.nextInt();
+            System.out.println("Type another integer: ");
+            n2 = scan.nextInt();
 
-        System.out.println("Type another integer: ");
-        n2 = scan.nextInt();
+            res = n1 + n2;
 
-        res = n1 + n2;
+            System.out.printf("Sum of %d with %d is equal to: %d\n", n1, n2, res);
+        }
 
-        System.out.printf("Sum of %d with %d is equal to: %d", n1, n2, res);
+        System.out.println("end of the program");
 
     }
 }
---
+++ Exercise4/Exercise4.java
@@ -0,0 +1,19 @@
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        char letter = 'a';
+        System.out.println(letter);
+
+        char value = 66;
+        System.out.println(value);
+
+        value = (char) (value + 1);
+        System.out.println(value);
+
+        String phrase = "This is a example phrase ";
+        System.out.println(phrase);
+
+        phrase = phrase + 2022;
+        System.out.println(phrase);
+    }
+}
